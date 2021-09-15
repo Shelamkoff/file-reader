a90b4f6d57828ac942ad6986e3ffcfedc17d125b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-16 01:13:58 +0200

    Update file-reader.js

--- file-reader.js
+++ file-reader.js
@@ -4,26 +4,29 @@ class AsyncFileReader {
     }
 
     /**
-     *
+     * 
      * @returns {string}
+     * @private
      */
-    static asBinary() {
+    static _asBinary() {
         return 'binary'
     }
 
     /**
-     *
+     * 
      * @returns {string}
+     * @private
      */
-    static asText() {
+    static _asText() {
         return 'text';
     }
 
     /**
-     *
+     * 
      * @returns {string}
+     * @private
      */
-    static asBuffer() {
+    static _asBuffer() {
         return 'buffer'
     }
 
@@ -41,7 +44,7 @@ class AsyncFileReader {
      * @returns {Promise<string>}
      */
     async readAsBinaryString(file) {
-        return this._read(file, this._asBinary)
+        return this._read(file, AsyncFileReader._asBinary())
     }
 
     /**
@@ -51,7 +54,7 @@ class AsyncFileReader {
      * @returns {Promise<string>}
      */
     async readAsText(file, encoding = 'UTF-8') {
-        return this._read(file, this._asText, encoding)
+        return this._read(file, AsyncFileReader._asText(), encoding)
     }
 
     /**
@@ -60,13 +63,13 @@ class AsyncFileReader {
      * @returns {Promise<ArrayBuffer>}
      */
     async readAsArrayBuffer(file) {
-        return this._read(file, this._asBuffer)
+        return this._read(file, AsyncFileReader.asBuffer())
     }
 
     /**
      *
      * @param {File|Blob} file
-     * @param {string<asBuffer|asText|asBinary>} as
+     * @param {string<_asBuffer|_asText|_asBinary>} as
      * @param {string} encoding
      * @returns {Promise<string|ArrayBuffer>}
      * @private
@@ -77,13 +80,13 @@ class AsyncFileReader {
             reader.onload = (e) => resolve(e.target.result)
             reader.onerror = (e) => reject(e.target.error)
             switch (as) {
-                case AsyncFileReader.asText():
+                case AsyncFileReader._asText():
                     reader.readAsText(file, encoding);
                     break;
-                case AsyncFileReader.asBinary():
+                case AsyncFileReader._asBinary():
                     reader.readAsBinaryString(file);
                     break;
-                case AsyncFileReader.asBuffer():
+                case AsyncFileReader._asBuffer():
                     reader.readAsArrayBuffer(file);
                     break;
                 default:
